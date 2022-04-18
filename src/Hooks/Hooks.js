a18362730e60176c71bc3fbb3ad80c4f2cc98b58
zarindev <mdzarinjerry@gmail.com>
2022-04-18 14:41:13 +0600

    first

---
+++ src/Hooks/Hooks.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+function Hooks() {
+  const [data, setData] = useState("");
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    axios
+    .get("https://jsonplaceholder.typicode.com/comments")
+    .then((response)=>{
+      setData(response.data[0].email);
+      console.log("API was called");
+    });
+  }, []);
+  return (
+    <div>Hello {data}
+    <h1>{data}</h1>
+    <h1>{count}</h1>
+    <button
+    onClick={() => {
+      setCount(count + 1);
+    }}>
+      Click
+    </button>
+    </div>
+  )
+}
+
+export default Hooks
